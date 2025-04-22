405754ebefc30030f01405272f766a0f039d0895
darulannas <darulannas7@gmail.com>
2025-04-23 05:26:50 +0700

    penambahan uji coba insert habit via log di mainactivity

--- app/src/main/java/com/acevy/habit_tracker/data/local/database/AppDatabase.kt
+++ app/src/main/java/com/acevy/habit_tracker/data/local/database/AppDatabase.kt
@@ -4,7 +4,7 @@ import androidx.room.*
 import com.acevy.habit_tracker.data.local.dao.HabitDao
 import com.acevy.habit_tracker.data.model.HabitEntity
 
-@Database(entities = [HabitEntity::class], version = 2)
+@Database(entities = [HabitEntity::class], version = 1)
 @TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun habitDao(): HabitDao
