b29d12a2e4b00c4f909fc2ec05309a28fa66e13e
devymutiara <namikazepian@gmail.com>
2025-04-28 23:23:17 +0700

    fix: put padding in entire apps for phone statusbar and navigationbar

--- app/src/main/java/com/acevy/habit_tracker/HabitTrackerApp.kt
+++ app/src/main/java/com/acevy/habit_tracker/HabitTrackerApp.kt
@@ -2,8 +2,14 @@ package com.acevy.habit_tracker
 
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.acevy.habit_tracker.ui.navigation.AppNavHost
@@ -14,8 +20,12 @@ fun HabitTrackerApp(
     modifier: Modifier = Modifier,
     navController: NavHostController = rememberNavController()
 ) {
-    AppNavHost(
-        navController = navController,
+    Surface(
         modifier = modifier
-    )
+            .fillMaxSize()
+            .navigationBarsPadding()
+            .statusBarsPadding()
+    ) {
+        AppNavHost(navController = navController)
+    }
 }
